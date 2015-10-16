b0ea325e6ea854bd20afe1ed417887f64732d8ea
Alexander Savchuk <alexander.savchuk@xero.com>
2015-10-16 13:28:57 +1300

    Add Kinesis Firehose sink for .NET 4.0
    This is essentially copy-pasted from Kinesis Streams sink,
    could certainly use some refactoring love.

---
+++ src/Serilog.Sinks.Amazon.Kinesis.Firehose/KinesisFirehoseLoggerConfigurationExtensions.cs
@@ -0,0 +1,102 @@
+﻿// Copyright 2014 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Amazon.KinesisFirehose;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.Amazon.Kinesis;
+using Serilog.Sinks.Amazon.Kinesis.Firehose;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Adds the WriteTo.AmazonKinesisFirehose() extension method to <see cref="LoggerConfiguration"/>.
+    /// </summary>
+    public static class KinesisFirehoseLoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Adds a sink that writes log events as documents to Amazon Kinesis Firehose.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="options"></param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration AmazonKinesisFirehose(
+            this LoggerSinkConfiguration loggerConfiguration,
+            KinesisFirehoseSinkOptions options)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
+            if (options == null) throw new ArgumentNullException("options");
+
+            ILogEventSink sink;
+            if (options.BufferBaseFilename == null)
+            {
+                sink = new KinesisFirehoseSink(options);
+            }
+            else
+            {
+                sink = new DurableKinesisFirehoseSink(options);
+            }
+
+            return loggerConfiguration.Sink(sink, options.MinimumLogEventLevel ?? LevelAlias.Minimum);
+        }
+
+
+        /// <summary>
+        /// Adds a sink that writes log events as documents to Amazon Kinesis.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="kinesisFirehoseClient"></param>
+        /// <param name="streamName"></param>
+        /// <param name="bufferBaseFilename"></param>
+        /// <param name="bufferFileSizeLimitBytes"></param>
+        /// <param name="batchPostingLimit"></param>
+        /// <param name="bufferLogShippingInterval"></param>
+        /// <param name="period"></param>
+        /// <param name="minimumLogEventLevel"></param>
+        /// <param name="onLogSendError"></param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static LoggerConfiguration AmazonKinesisFirehose(
+            this LoggerSinkConfiguration loggerConfiguration,
+            IAmazonKinesisFirehose kinesisFirehoseClient,
+            string streamName,
+            string bufferBaseFilename = null,
+            int? bufferFileSizeLimitBytes = null,
+            int? batchPostingLimit = null,
+            TimeSpan? bufferLogShippingInterval = null,
+            TimeSpan? period = null,
+            LogEventLevel? minimumLogEventLevel = null,
+            EventHandler<LogSendErrorEventArgs> onLogSendError = null)
+        {
+            if (kinesisFirehoseClient == null) throw new ArgumentNullException("kinesisFirehoseClient");
+            if (streamName == null) throw new ArgumentNullException("streamName");
+
+            var options = new KinesisFirehoseSinkOptions(kinesisFirehoseClient: kinesisFirehoseClient, streamName: streamName)
+            {
+                BufferFileSizeLimitBytes = bufferFileSizeLimitBytes,
+                BufferBaseFilename = bufferBaseFilename,
+                BufferLogShippingInterval = KinesisFirehoseSinkOptions.DefaultBufferLogShippingInterval,
+                Period = period ?? KinesisFirehoseSinkOptions.DefaultPeriod,
+                BatchPostingLimit = batchPostingLimit ?? KinesisFirehoseSinkOptions.DefaultBatchPostingLimit,
+                MinimumLogEventLevel = minimumLogEventLevel ?? LevelAlias.Minimum,
+                OnLogSendError = onLogSendError
+            };
+
+            return AmazonKinesisFirehose(loggerConfiguration, options);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.Amazon.Kinesis.Firehose/Sinks/DurableKinesisFirehoseSink.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2014 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.RollingFile;
+
+namespace Serilog.Sinks.Amazon.Kinesis.Firehose
+{
+    class DurableKinesisFirehoseSink : ILogEventSink, IDisposable
+    {
+        readonly HttpLogShipper _shipper;
+        readonly RollingFileSink _sink;
+
+        public DurableKinesisFirehoseSink(KinesisFirehoseSinkOptions options)
+        {
+            var state = KinesisFirehoseSinkState.Create(options);
+
+            if (string.IsNullOrWhiteSpace(options.BufferBaseFilename))
+            {
+                throw new ArgumentException("Cannot create the durable Amazon Kinesis Firehose sink without a buffer base file name.");
+            }
+
+            _sink = new RollingFileSink(
+               options.BufferBaseFilename + "-{Date}.json",
+               state.DurableFormatter,
+               options.BufferFileSizeLimitBytes,
+               null);
+
+            _shipper = new HttpLogShipper(state);
+
+            if (options.OnLogSendError != null) {
+                _shipper.LogSendError += options.OnLogSendError;
+            }
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            _sink.Emit(logEvent);
+        }
+
+        public void Dispose()
+        {
+            _sink.Dispose();
+            _shipper.Dispose();
+        }
+    }
+}
