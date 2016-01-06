b6d7e4317ccb669854399a616074055e07671af5
Justin Thirkell <justin.thirkell@xero.com>
2016-01-06 14:26:33 +1300

    reinstalled nuget packages to get correct framework targeting

--- src/Serilog.Sinks.Amazon.Kinesis/CustomJsonFormatter.cs
+++ src/Serilog.Sinks.Amazon.Kinesis/CustomJsonFormatter.cs
@@ -4,13 +4,22 @@ using Serilog.Formatting.Json;
 
 namespace Serilog.Sinks.Amazon.Kinesis
 {
+    /// <summary>
+    /// wut
+    /// </summary>
     public class CustomJsonFormatter : JsonFormatter
     {
+    /// <summary>
+    /// wut
+    /// </summary>
         public CustomJsonFormatter(bool omitEnclosingObject = false, string closingDelimiter = null, bool renderMessage = false, IFormatProvider formatProvider = null)
             : base(omitEnclosingObject, closingDelimiter, renderMessage, formatProvider)
         {
         }
 
+    /// <summary>
+    /// wut
+    /// </summary>
         protected override void WriteTimestamp(DateTimeOffset timestamp, ref string precedingDelimiter, TextWriter output)
         {
             output.Write(precedingDelimiter);
