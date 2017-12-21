7b517b3513f310319efb661b7d409e661a5e43ef
Spencer Langley <spencer.langley@csgi.com>
2017-12-21 14:35:40 -0600

    Add signature to assembly on build

--- src/Serilog.Sinks.Amazon.Kinesis/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Amazon.Kinesis/Properties/AssemblyInfo.cs
@@ -34,5 +34,24 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Serilog.Sinks.Amazon.Kinesis.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Required to do AutoFixture magic on internal interfaces
+//[assembly: InternalsVisibleTo("Serilog.Sinks.Amazon.Kinesis.Tests")]
+[assembly: InternalsVisibleTo("Serilog.Sinks.Amazon.Kinesis.Tests, PublicKey=00240000048000009400000006020000" +
+                                                                    "00240000525341310004000001000100" +
+                                                                    "FB8D13FD344A1C6FE0FE83EF33C1080B" +
+                                                                    "F30690765BC6EB0DF26EBFDF8F21670C" +
+                                                                    "64265B30DB09F73A0DEA5B3DB4C9D18D" +
+                                                                    "BF6D5A25AF5CE9016F281014D79DC3B4" +
+                                                                    "201AC646C451830FC7E61A2DFD633D34" +
+                                                                    "C39F87B81894191652DF5AC63CC40C77" +
+                                                                    "F3542F702BDA692E6E8A9158353DF189" +
+                                                                    "007A49DA0F3CFD55EB250066B19485EC")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=00240000048000009400000006020000002" +
+                                                                    "40000525341310004000001000100c547ca" +
+                                                                    "c37abd99c8db225ef2f6c8a3602f3b3606c" +
+                                                                    "c9891605d02baa56104f4cfc0734aa39b93" +
+                                                                    "bf7852f7d9266654753cc297e7d2edfe0ba" +
+                                                                    "c1cdcf9f717241550e0a7b191195b7667bb" +
+                                                                    "4f64bcb8e2121380fd1d9d46ad2d92d2d15" +
+                                                                    "605093924cceaf74c4861eff62abf69b929" +
+                                                                    "1ed0a340e113be11e6a7d3113e92484cf70" +
+                                                                    "45cc7")] // Required to do AutoFixture magic on internal interfaces
